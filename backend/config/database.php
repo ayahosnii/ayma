20d19abbf904caef780f3babd956c50f5806a5cc
Magd El Zalameh <Magd El Zalameh>
2024-10-16 22:42:50 +0300

    categories/subcategories edits

--- backend/config/database.php
+++ backend/config/database.php
@@ -97,9 +97,9 @@ return [
             'driver' => 'mongodb',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '27017'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'options' => [
                 'database' => env('DB_MONGODB_DATABASE', 'admin'),
             ],
